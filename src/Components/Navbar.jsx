68cdece909031d5526d486fcb7add90d5d302503
umutesililiane <umutesililiane4@gmail.com>
2023-06-18 23:43:29 +0530

    navigation bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { UseState } from "react";
+
+import { logo } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo}  />
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
